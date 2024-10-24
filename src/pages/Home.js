37fdd7bc3657161ca126feb6675397730254991f
Guilherme <guicsilvash@gmail.com>
2024-10-24 20:05:59 -0300

    @guipdg | feat: cria pagina Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React from 'react';
+import { UnauthNavbar } from "../components/Navbar";
+import '../styles/Home.css'; // Importando o arquivo CSS
 
 const Home = () => {
+    // Função que será executada ao clicar no botão
+    const handleClick = () => {
+      alert("Iniciar o processo de cadastro!");
+    };
+  
     return (
-        <div>
-            <h1>Aqui é a página HOME!</h1>
+      <><UnauthNavbar />
+  
+        <div className="header">
+          <h1 className="main-text">
+            Descomplique o resultado <br/>dos seus exames <strong>agora, <br/>sem custo.</strong>
+          </h1>
+          <div className="button-container">
+            <button className="button">entrar</button>
+            <button className={`button register-button`}>cadastrar</button>
+          </div>
         </div>
-    )
-}
-
-export default Home;
+  
+        {/* Botão principal que chama a função handleClick */}
+        <button className="main-button" onClick={handleClick}>
+          experimente já
+        </button>
+      </>
+    );
+  };
+  
+  export default Home;
