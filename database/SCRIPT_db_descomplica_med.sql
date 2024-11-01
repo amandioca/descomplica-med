4b8c3d100fba6ecb6101027bbbeef2bf6bd2aaa4
Amanda Amorim <amandaamorim.ctt@gmail.com>
2024-10-31 22:23:33 -0300

    @amandioca | refactor: renomeia banco de dados

--- database/SCRIPT_db_descomplica_med.sql
+++ database/SCRIPT_db_descomplica_med.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema descomplica_med
+-- Schema descomplicamed-db
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema descomplica_med
+-- Schema descomplicamed-db
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `descomplica_med` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `descomplica_med` ;
+CREATE SCHEMA IF NOT EXISTS `descomplicamed-db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `descomplicamed-db` ;
 
 -- -----------------------------------------------------
--- Table `descomplica_med`.`users`
+-- Table `descomplicamed-db`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `descomplica_med`.`users` (
+CREATE TABLE IF NOT EXISTS `descomplicamed-db`.`users` (
   `cpf` CHAR(11) NOT NULL,
   `full_name` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
@@ -29,9 +29,9 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `descomplica_med`.`chat_history`
+-- Table `descomplicamed-db`.`chat_history`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `descomplica_med`.`chat_history` (
+CREATE TABLE IF NOT EXISTS `descomplicamed-db`.`chat_history` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `sender` VARCHAR(50) NOT NULL,
   `message_text` TEXT NULL DEFAULT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `descomplica_med`.`chat_history` (
   INDEX `user_cpf` (`user_cpf` ASC) VISIBLE,
   CONSTRAINT `chat_history_ibfk_1`
     FOREIGN KEY (`user_cpf`)
-    REFERENCES `descomplica_med`.`users` (`cpf`))
+    REFERENCES `descomplicamed-db`.`users` (`cpf`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
