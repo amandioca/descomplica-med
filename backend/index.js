4c8240af07637b7c740d4e6449160ea7062c0ed5
Amanda Amorim <amandaamorim.ctt@gmail.com>
2024-11-03 00:37:13 -0300

    @amandioca | feat: adiciona conexao com db e endpoints getUsers e getUserByCpf

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,38 @@
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 require('dotenv').config();
 const express = require('express');
+const dbService = require('./dbService');
 
 const app = express();
 app.use(express.json());
 
-const OPENAI_KEY = process.env.OPENAI_KEY
-const PORT = 3000
-
-app.get('/hello-world', (req, res) => {
-    res.send("hello world!")
-})
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
-})
+});
+
+app.get('/hello-world', (req, res) => {
+    res.send({'message':'hello world!'})
+});
+
+app.get('/users', async (req, res) =>{
+    try {
+        const data = await dbService.getUsers();
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({'message':'Connection to the SQL server failed'})
+    }
+});
+
+app.get('/users/:cpf', async (req, res) =>{
+    try {
+        const user = await dbService.getUserByCpf(req.params.cpf);
+        user.length === 1
+            ? res.json(user)
+            : res.status(404).send({'message':'User not found'});
+    } catch (err) {
+        res.status(500).json({'message':'Connection to the SQL server failed'})
+    }
+});
