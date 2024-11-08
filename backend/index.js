fe69dad903d45a06b41b2cc36c05fc8fb6e43db5
Amanda Amorim <amandaamorim.ctt@gmail.com>
2024-11-08 20:07:13 -0300

    @amandioca | refactor: substitui estrutura antiga com Router Express para melhorar modularidade

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,21 @@
 
 require('dotenv').config();
 const express = require('express');
-const dbService = require('./dbService');
 const gemini = require('./geminiService');
+const userRoutes = require('./userRoutes');
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+app.use('/users', userRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-});
+const PORT = 3000;
 
 app.get('/hello-world', (req, res) => {
     res.send({ message: 'hello world!' })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 });
 
 app.get('/response-gemini', async (req, res) => {
@@ -31,39 +33,3 @@ app.get('/response-gemini', async (req, res) => {
         res.status(500).json({ message: `Generic error: ${error}` });
     }
 });
-
-app.get('/users', async (req, res) => {
-    try {
-        const data = await dbService.getUsers();
-        res.json(data);
-    } catch (error) {
-        res.status(500).json({ message: 'Connection to the SQL server failed' })
-    }
-});
-
-app.get('/users/:cpf', async (req, res) => {
-    try {
-        const user = await dbService.getUserByCpf(req.params.cpf);
-        user.length === 1
-            ? res.json(user)
-            : res.status(404);
-    } catch (error) {
-        res.status(500).json({ message: 'Connection to the SQL server failed' })
-    }
-});
-
-app.post('/users', async (req, res) => {
-    const { cpf, fullName, password } = req.body;
-    try {
-        const createdUser = await dbService.createUser(cpf, fullName, password);
-        const { password: _, ...userData } = createdUser;
-        res.status(201).send(userData)
-    } catch (error) {
-        if (error.code === '23505')
-            res.status(409).send({ message: 'User with this CPF already exists' });
-        else if (error.code === '22001')
-            res.status(400).send({ message: error.message });
-        else
-            res.status(500).send({ message: error.message })
-    }
-})
---
+++ backend/userRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router();
+const dbService = require('./dbService');
+
+router.get('/', async (req, res) => {
+    try {
+        const data = await dbService.getUsers();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ message: 'Connection to the SQL server failed' })
+    }
+});
+
+router.get('/:cpf', async (req, res) => {
+    try {
+        const user = await dbService.getUserByCpf(req.params.cpf);
+        user.length === 1
+            ? res.json(user)
+            : res.status(404);
+    } catch (error) {
+        res.status(500).json({ message: 'Connection to the SQL server failed' })
+    }
+});
+
+router.post('/', async (req, res) => {
+    const { cpf, fullName, password } = req.body;
+    try {
+        const createdUser = await dbService.createUser(cpf, fullName, password);
+        const { password: _, ...userData } = createdUser;
+        res.status(201).send(userData)
+    } catch (error) {
+        if (error.code === '23505')
+            res.status(409).send({ message: 'User with this CPF already exists' });
+        else if (error.code === '22001')
+            res.status(400).send({ message: error.message });
+        else
+            res.status(500).send({ message: error.message })
+    }
+})
+
+module.exports = router;
