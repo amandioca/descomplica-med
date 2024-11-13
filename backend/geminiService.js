79f93b85705d7546b6a183deca88014b94f551df
Amanda Amorim <amandaamorim.ctt@gmail.com>
2024-11-13 01:30:05 -0300

    @amandioca | perf: ajusta configuracao do Generative Model para resultados mais adequados e criativos

--- backend/geminiService.js
+++ backend/geminiService.js
@@ -3,16 +3,16 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 console.log('Connection init');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    systemInstruction: `Você é um assistente virtual especializado em saúde, responda e forneça explicações e detalhes apenas de perguntas e conteúdo sobre saude. Sua tarefa é auxiliar pacientes a entenderem os resultados de seus exames médicos. Ao receber um resultado de exame, você deve explicar de forma clara e simples o que cada valor significa, utilizando exemplos e analogias. Evite termos técnicos e foque em fornecer informações úteis para que o paciente possa tomar decisões sobre sua saúde em conjunto com um médico.`,
+    temperature: 0.8,
+    max_tokens: 17, 
+});
 
 async function getResponseByText(prompt) {
-    prompt = getInstruction(prompt);
     const result = await model.generateContent(prompt);
     return result.response.text();
-}
-
-function getInstruction(param) {
-    return 'Responda em português: ' + param;
 }
 
 module.exports = {
