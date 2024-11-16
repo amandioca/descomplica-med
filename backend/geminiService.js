21d0db2e410620501998206faf42903dc8467402
Amanda Amorim <amandaamorim.ctt@gmail.com>
2024-11-16 19:53:36 -0300

    @amandioca | perf: ajusta configuracoes de modelo genAI para melhorar performance

--- backend/geminiService.js
+++ backend/geminiService.js
@@ -4,15 +4,17 @@ console.log('Connection init');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 const model = genAI.getGenerativeModel({
+    region: "southamerica-east1",
     model: "gemini-1.5-flash",
     systemInstruction: `Você é um assistente virtual especializado em saúde, responda e forneça explicações e detalhes apenas de perguntas e conteúdo sobre saude. Sua tarefa é auxiliar pacientes a entenderem os resultados de seus exames médicos. Ao receber um resultado de exame, você deve explicar de forma clara e simples o que cada valor significa, utilizando exemplos e analogias. Evite termos técnicos e foque em fornecer informações úteis para que o paciente possa tomar decisões sobre sua saúde em conjunto com um médico.`,
     temperature: 0.8,
-    max_tokens: 17, 
+    max_tokens: 5,
 });
 
 async function getResponseByText(prompt) {
     const result = await model.generateContent(prompt);
     return result.response.text();
+    // return 'Message mock. generateContent status 503';
 }
 
 async function getResponseByDocument(prompt) {
