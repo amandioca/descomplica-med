92c321696cf56f0cc9256756a743fdd4fbe07576
Amanda Amorim <amandaamorim.ctt@gmail.com>
2024-11-17 03:30:34 -0300

    @amandioca | refactor: cria pastas para melhorar organizacao do cod

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const dbService = require('./dbService');
+const dbService = require('../dbService');
 
 router.get('/', async (req, res) => {
     try {
