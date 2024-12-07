bca028a1081c4f1f66d1172b755292dce47795ec
Amanda Amorim <amandaamorim.ctt@gmail.com>
2024-12-07 03:15:19 -0300

    @amandioca | refactor: remove variaveis de estado nao utilizadas e ajustes style

--- frontend/src/forms/LoginForm.js
+++ frontend/src/forms/LoginForm.js
@@ -1,39 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import InputMask from 'react-input-mask';
 import { useLocation } from 'react-router-dom';
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/global.css'
 
 const LoginForms = () => {
-    const inputStyle = { backgroundColor: 'var(--color-gray-medium)', border: '1px solid var(--color-gray)', color: 'var(--color-white)' }
     const location = useLocation();
     const [cpf, setCpf] = useState('');
     const [pass, setPass] = useState('');
-    const [validationCpf, setValidationCpf] = useState(false);
 
     useEffect(() => {
         if (location.state?.cpf)
             setCpf(location.state.cpf);
     }, [location.state]);
 
-    const handleCpf = (input) => {
-        setCpf(input)
-
-        if (input.length === 14)
-            setValidationCpf(true);
-        else
-            setValidationCpf(false);
-    }
-
     return (
         <form style={{ marginTop: 40, marginBottom: 50 }}>
             <div class='mb-3'>
                 <label for='cpf' class='form-label'>CPF:</label>
                 <InputMask id='cpf'
-                    class='form-control form-input'
-                    style={inputStyle}
-                    onChange={(e) => handleCpf(e.target.value)}
+                    class='input form-control form-input'
+                    onChange={(e) => setCpf(e.target.value)}
                     value={cpf}
                     type='text'
                     mask='999.999.999-99'
@@ -44,9 +31,8 @@ const LoginForms = () => {
                 <label for='pass' class='form-label'>Senha:
                 </label>
                 <input id='pass'
-                    class='form-control form-input'
-                    style={inputStyle}
-                    onChange={(e) => handlePassword(e.target.value)}
+                    class='input form-control form-input'
+                    onChange={(e) => setPass(e.target.value)}
                     value={pass}
                     type='password'
                     placeholder='••••••••'
